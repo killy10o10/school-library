93e0c80885568163a03135a6e013fe343a49caa8
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-11-01 17:53:54 +0000

    Fix rubocop warnings

--- person.rb
+++ person.rb
@@ -9,13 +9,12 @@ class Person
     @parent_permission = parent_permission
   end
 
-  def is_of_age? 
+  def of_age?
     @age >= 18
   end
-  private: is_of_age?
-  
+  private :is_of_age?
+
   def can_use_services?
     is_of_age? || parent_permission
   end
-
 end
