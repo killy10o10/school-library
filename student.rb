303dde4a85ed65b08954a2d266e64aff7e2aa643
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-11-05 12:21:47 +0000

    Create console app

--- student.rb
+++ student.rb
@@ -1,8 +1,6 @@
 require './person'
 
 class Student < Person
-  attr_accessor :classroom
-
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     @classroom = classroom
 
@@ -11,10 +9,5 @@ class Student < Person
 
   def play_hooky
     '¯\(ツ)/¯'
-  end
-
-  def add_classroom(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
